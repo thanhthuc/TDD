65a6049ba0fa3f8d83397258123aa6a8aa9404ec
thanh thuc swift <thanhthucbkinfo@yahoo.com.vn>
2017-01-13 01:32:50 +0700

    Some change

---
+++ TDD/GreetButton.swift
@@ -0,0 +1,21 @@
+//
+//  GreetButton.swift
+//  TDD
+//
+//  Created by Nguyễn Thành Thực on 1/12/17.
+//  Copyright © 2017 Nguyen Thanh Thuc. All rights reserved.
+//
+
+import UIKit
+
+class GreetButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TDD/HowViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HowToPlayViewController.swift
+//  TDD
+//
+//  Created by Nguyễn Thành Thực on 1/13/17.
+//  Copyright © 2017 Nguyen Thanh Thuc. All rights reserved.
+//
+
+import UIKit
+
+class HowToPlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
